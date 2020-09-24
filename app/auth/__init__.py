b6364509f173f1be6cc3e62eb9d32eec0c87118a
Diego Cabrera Nieto <a01633860@itesm.mx>
2020-09-24 15:15:03 -0500

    Authentication models created, Product models created, CRUD operations for products available, and Signup and Login available
    Signed-off-by: Diego Cabrera Nieto <a01633860@itesm.mx>

---
+++ app/auth/__init__.py
@@ -0,0 +1,28 @@
+from app import db
+from app.auth.models import User
+
+def createUser(username, email, password):
+  try:
+    nameOnDatabase = User.query.filter_by(username=username).first()
+    mailOnDatabase =  User.query.filter_by(email=email).first()
+    print(nameOnDatabase)
+
+    if nameOnDatabase: return 'Username is already taken', 500
+    if mailOnDatabase: return 'Mail is already taken', 500
+    
+    user = User(username=username, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return 'User created', 200
+  except Exception:
+    return 'Error on server', 500
+
+def searchUser(username, password):
+  user = User.query.filter_by(username=username).first()
+  if user:
+    if user.password == password:
+      return 'Authorized Login', 200
+    else:
+      return 'Wrong Password', 500
+  else:
+    return 'Wrong Username', 500
---
+++ app/products/__init__.py
@@ -0,0 +1,46 @@
+from flask import jsonify
+from app import db
+from app.products.models import Product
+
+def getProduct(id):
+  query = db.session.query(Product)
+  query = query.filter(Product.id == id)
+  retrievedProduct = query.first()
+  return retrievedProduct.as_dict()
+
+def deleteProduct(id):
+  query = db.session.query(Product)
+  query = query.filter(Product.id == id)
+  retrievedProduct = query.first()
+  db.session.delete(retrievedProduct)
+  return {'Deleted Object': retrievedProduct.as_dict()}
+
+def getProducts(params):
+  query = db.session.query(Product)
+  for k, v in params.items():
+    if k != 'price' and v:
+      query = query.filter(getattr(Product,k) == v)
+    elif v:
+      query = query.filter(getattr(Product,k) >= v[0])\
+        .filter(getattr(Product,k) <= v[1])
+  rows = query.all()
+
+  products = [row.as_dict() for row in rows]
+  
+  return products
+
+def createProduct(productData):
+  try:
+    product = Product(
+      name = productData['name'],
+      style = productData['style'],
+      color = productData['color'],
+      gender = productData['gender'],
+      price = productData['price'],
+      description = productData['description'],
+    )
+    db.session.add(product)
+    db.session.commit()
+    return 'Product Created Successfuly', 200
+  except Exception:
+    return 'Error while creating the product', 500
