963bc42e5ddb16bb17eb62cd91532de987bc77d6
DiegoCabreraN <diegocabreranieto@gmail.com>
2021-03-21 13:24:24 -0600

    CORS Error

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sunshop.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
